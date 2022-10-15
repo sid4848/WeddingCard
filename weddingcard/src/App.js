02f8801ece9b7bc8f694667c28830b32a74e4a39
sid4848 <shitalsiddhagavali4848@gmail.com>
2022-10-15 11:53:24 +0530

    commit

--- weddingcard/src/App.js
+++ weddingcard/src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Navbar, Header, Features } from "./containers";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Header></Header>
+      <Features></Features>
+    </div>
+  );
 }
 
 export default App;
