8185a2df720979f6623eba7e1616585e119dd471
sid4848 <shitalsiddhagavali4848@gmail.com>
2022-10-16 10:39:31 +0530

    commit

--- weddingcard/src/containers/header/Header.jsx
+++ weddingcard/src/containers/header/Header.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import Slider from "react-slick";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import Page1 from "../../assets/Page1.jpg";
+import Page2 from "../../assets/Page2.jpg";
+import Page3 from "../../assets/Page3.jpg";
 import "./header.css";
+import { useState } from "react";
+
+const images = [Page1, Page2, Page3];
+
 const Header = () => {
-  return <div>Header</div>;
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 600,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
+  return (
+    <>
+      <div className="wc__header">
+        <h1>Digital card details</h1>
+      </div>
+      <div className="wc__header-container">
+        <div className="wc__header-container_cards">
+          <Slider {...settings}>
+            {images.map((img, idx) => (
+              <div
+                className={idx === imageIndex ? "slide activeSlide" : "slide"}
+                key={idx}
+              >
+                <img src={img} alt={img} />
+              </div>
+            ))}
+          </Slider>
+        </div>
+        <div className="wc__header-container_content">
+          <h1>Crad title</h1>
+          <p>
+            $499 <span>75%OFF</span>
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+            incidunt, amet fugit aspernatur et facilis asperiores expedita! Eius
+            veritatis, odit quis cumque earum neque doloribus omnis consequatur
+            adipisci laborum eveniet, eligendi minus pariatur culpa rerum illum
+            voluptates natus sequi. Rem!
+          </p>
+          <button type="submit">Try this card</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- weddingcard/src/containers/navbar/Navbar.jsx
+++ weddingcard/src/containers/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import Logo from "../../assets/Logo.png";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="wc__navbar">
+      <p className="wc__navbar-links">
+        <a href="#">
+          <img src={Logo} alt="logo..." />
+        </a>
+      </p>
+      <p className="wc__navbar-links">
+        <a href="#">Wedding Cards</a>
+      </p>
+      <p className="wc__navbar-links">
+        <a href="#">Birthday Cards</a>
+      </p>
+      <p className="wc__navbar-links">
+        <a href="#">Aniversery Crads</a>
+      </p>
+      <p className="wc__navbar-links">
+        <a href="#">Login | </a>
+        <a href="#">Signin</a>
+      </p>
+    </div>
+  );
 };
 
 export default Navbar;
